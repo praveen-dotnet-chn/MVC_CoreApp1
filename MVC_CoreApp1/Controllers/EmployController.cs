1d6a6d72f46606800052bcdd22daea21d65b4942
Praveen.S3 <Praveen.S3@ENCOPDBANLT1776>
2025-10-07 12:14:56 +0530

    Add project files.

---
+++ MVC_CoreApp1/Controllers/EmployController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CoreApp1.Models;
+
+namespace MVC_CoreApp1.Controllers
+{
+    public class EmployController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employ emp = new Employ()
+            {
+                Id = 1,
+                Name = "Pandu",
+                Salary = 99999
+            };
+
+            return View(emp);
+        }
+    }
+}
---
+++ MVC_CoreApp1/Models/Employ.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_CoreApp1.Models
+{
+    public class Employ
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public double Salary { get; set; }
+
+    }
+}
